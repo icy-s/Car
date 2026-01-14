7c5343d7937f13e1d2881e4ad4a2078fabf05a51
icy-s <evgeny.tailov@gmail.com>
2026-01-14 15:55:58 +0200

    Car.Core successfully added to project

---
+++ Car.Core/Dto/CarDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Car.Core.Dto
+{
+    public class CarDto
+    {
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int? Year { get; set; }
+        public int? Mileage { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
