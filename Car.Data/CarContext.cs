a22607478135d70126d42c8e1532f454afed6eb4
icy-s <evgeny.tailov@gmail.com>
2026-01-15 11:03:03 +0200

    car context

---
+++ Car.Data/CarContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Car.Data
+{
+    public class CarContext : IdentityDbContext<ApplicationUser>
+    {
+        public CarContext(DbContextOptions<CarContext> options)
+        : base(options) { }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
