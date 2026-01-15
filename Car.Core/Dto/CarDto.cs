cf36cef21de62495ab1ee38188a7ff4a635ba668
icy-s <evgeny.tailov@gmail.com>
2026-01-15 10:56:53 +0200

    car controller

--- Car.Core/Dto/CarDto.cs
+++ Car.Core/Dto/CarDto.cs
@@ -2,9 +2,10 @@
 {
     public class CarDto
     {
-        public string? Make { get; set; }
-        public string? Model { get; set; }
-        public int? Year { get; set; }
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
         public int? Mileage { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? ModifiedAt { get; set; }
--- Car/Controllers/CarController.cs
+++ Car/Controllers/CarController.cs
@@ -4,9 +4,170 @@ namespace Car.Controllers
 {
     public class CarController : Controller
     {
+        private readonly CarContext _context;
+        private readonly ICarServices _carServices;
+
+        public CarController
+            (
+                CarContext context,
+                ICarServices carServices
+            )
+        {
+            _context = context;
+            _carServices = _context.CarServices;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Cars
+                .Select(x => new CarsIndexViewModel
+                {
+                    Id = x.Id,
+                    Make = x.Make,
+                    Model = x.Model,
+                    Year = x.Year,
+                    Mileage = x.Mileage
+                });
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            CarCreateUpdateViewModel result = new();
+            return View("CreateUpdate", result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CarCreateUpdateViewModel vm)
+        {
+            var dto = new CarDto()
+            {
+                Id = vm.Id,
+                Make = vm.Make,
+                Model = vm.Model,
+                Year = vm.Year,
+                Mileage = vm.Mileage,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt
+            };
+
+            var result = await _carServices.Create(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var car = await _carServices.DetailAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new CarDeleteViewModel();
+
+            vm.Id = car.Id;
+            vm.Make = car.Make;
+            vm.Model = car.Model;
+            vm.Year = car.Year;
+            vm.Mileage = car.Mileage;
+            vm.CreatedAt = car.CreatedAt;
+            vm.ModifiedAt = car.ModifiedAt;
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmation(Guid id)
+        {
+            var deleted = await _carServices.Delete(id);
+
+            if (deleted == null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var update = await _carServices.DetailAsync(id);
+
+            if (update == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new CarCreateUpdateViewModel();
+
+            vm.Id = update.Id;
+            vm.Make = update.Make;
+            vm.Model = update.Model;
+            vm.Year = update.Year;
+            vm.Mileage = update.Mileage;
+            vm.CreatedAt = update.CreatedAt;
+            vm.ModifiedAt = update.ModifiedAt;
+
+            return View("CreateUpdate", vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(CarCreateUpdateViewModel vm)
+        {
+            var dto = new CarDto()
+            {
+                Id = vm.Id,
+                Make = vm.Make,
+                Model = vm.Model,
+                Year = vm.Year,
+                Mileage = vm.Mileage,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt
+            };
+
+            var result = await _carServices.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details (Guid id)
+        {
+            var car = await _carServices.DetailAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new CarDetailsViewModel();
+
+            vm.Id = car.Id;
+            vm.Make = car.Make;
+            vm.Model = car.Model;
+            vm.Year = car.Year;
+            vm.Mileage = car.Mileage;
+            vm.CreatedAt = car.CreatedAt;
+            vm.ModifiedAt = car.ModifiedAt;
+
+            return View(vm);
         }
     }
 }
