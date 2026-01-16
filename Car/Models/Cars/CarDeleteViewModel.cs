9373ab645674a621077e70a3c67c8452764a48ce
icy-s <evgeny.tailov@gmail.com>
2026-01-16 12:05:29 +0200

    car models

---
+++ Car/Models/Cars/CarDeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Car.Models.Cars
+{
+    public class CarDeleteViewModel
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int? Mileage { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
---
+++ Car/Models/Cars/CarsIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Car.Models.Cars
+{
+    public class CarsIndexViewModel
+    {
+        public Guid Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int? Mileage { get; set; }
+    }
+}
